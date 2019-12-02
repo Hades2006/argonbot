4caeef3a60a192824da59c4ecdcddb7834b119b9
Hades2006 <51516142+Hades2006@users.noreply.github.com>
2019-12-02 04:48:39 +0300

    Add files via upload

---
+++ js/util.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function(){
+	let img = `                                     .-://+ooossssssooo+//:-.\`
+                               \`.:/osyyyyyyyyyyyyyyyyyyyyyyyyyyso+:-\`
+                           .:+oyyyyyyyyyyyyyyyyyyyyyyso++/:---.....---.\`
+                       \`-/syyyyyyyyyyyyyyyyyyyyys+/-.\`
+                     -+syyyyyyyyyyyyyyyyyyyys+:.\`
+                  ./syyyyyyyyyyyyyyyyyyyys+-\`
+                -+syyyyyyyyyyyyyyyyyyyyo:\`
+              -oyyyyyyyyyyyyyyyyyyyyyo-\`
+            .+yyyyyyyyyyyyyyyyyyyyyo-
+          \`/syyyyyyyyyyyyyyyyyyyys:\`
+         -oyyyyyyyyyyyyyyyyyyyyyo.    \`
+        /syyyyyyyyyyyyyyyyyyyys/\`   \`/s+
+      \`+yyyyyyyyyyyyyyyyyyys+:.\`\`.-:syyy+
+     \`oyyyyyyyyyyyyyyysoo+:\`\`-+osyyyyyyyy+.
+    \`oyyyyyyyyyyyyyyy+.\`..-/syyyyyyyyyyyyyy+.
+    +yyyyyyyyyyyyyyyy- /yyyyyyyyyyyyyyo--+yy+
+   /yyyyyyyyyyyyyyyyy+ .syyyyyyyyyyyyy/  -yy/
+  .yyyyyyyyyyyyyyyyyyy/ .syyys+osyyyyyo./yyo\`                 \`.:/++++//:.\`\`
+  oyyyyyyyyyyyyyyyyyyyy- /yyy/  \`+yyyyyyyys\`                 -oyo/:::/+osyyo+:.\`
+ -yyyyyyyyyyyyyyyyyyyyy/ -yyyo:-:/yyyyyyyyy+.               .yy/        \`:syyyys/.\`
+ oyyyyyyyyyyyyyyyyyyyyys. /syyyyyyyyyyyyyyyyy/\`             /yy\`          \`/syyyyys:\`
+.yyyyyyyyyyyyyyyyyyyyyyys/  .-:///::/+yyyyyyyys.            -yy:            \`:oyyyyys:
+:yyyyyyyyyyyyyyyyyyyyyyyyy\`         \`.syyyyyyyyy-            :yy/\`             \`:+syyyo\`
++yyyyyyyyyyyyyyyyyyyyyyyyy-     -/+osyyyyyyyyyyyy-            .+ss/\`              \`-/os:
+oyyyyyyyyyyyyyyyyyyyyyyyyy/    :yyyyyyyyyyyyyyyyys\`             \`-+so:.
+oyyyyyyyyyyyyyyyyyys+//+sys     \`.-:++oyyyyyyyyyyy-                 -oyo:\`
+oyyyyyyyyyyyyyyyyo. \`--.  .     \`+yyyyyyyyyyyyyyyy-                   ./yy/\`
++yyyyyyyyyyyyyyyy. /yyyyys+:-\`  /yyyyyyyyyyyyyyyyy\`                     \`+ys.
+:yyyyyyyyyyyyyyyy. /syyyyyyyyyo:oyyyyyyyyyyyyyyyy/                        /yy-                     \`
+.yyyyyyyyyyyyyyyys/-.\`-/+syyyyyyyyyyyyyyyyyyyyyy+                          /ys\`                   .-
+ oyyyyyyyyyyyyyyyyyyss+:.\`.:oyyyyyyyyyyyyyyyyyy/                           \`sy:                  .o\`
+ -yyyyyyyyyyyyyyyyyyyyyys\` :syyyyyyyyyyyyyysyyy+\`                           +y+                 .s+
+  oyyyyyyyyyyyyyyyyyyyyy/ -yyyyyyyyyys+/-..\`-syyo.                          oy/                :ss\`
+  .yyyyyyyyyyyyyyyyyyyyy+ -yyyyyyyyys:       \`/sys/\`                       .yy.              .+yy:
+   /yyyyyyyyyyyyyyyyyyys+\` -oyyyyyyys:         \`/sys/.                    -sy+             \`/syyo
+    +yyyyyyyyyyyyyyys+-\`\`-/osyyyyso:\`..          \`-+sso:.\`             \`-+yy/\`           \`:syyys\`
+    \`oyyyyyyyyyyyyo:\`\`-+syyyyys+:..-+ss+.\`          \`-+osso/:-......-:+sys+.           ./syyyys.
+     \`oyyyyyyyyyy/\`\`/syyyyys+:..:+syyyyyyo:.           \`.:/oosssssssso+:.\`          \`-+syyyyys.
+      \`+yyyyyyyys \`syyyys+-..:osyyyyyyyyyyyso/.\`            \`\`.....\`\`           \`.:+syyyyyyyo.
+        :syyyyyyy- :oso:..:osyyyyyyyyyyyyyyyyyyso/-.\`                      \`.-:+oyyyyyyyyyy+\`
+         -oyyyyyyy+-...-+syyyyyyyyyyyyyyyyyyyyyyyyyyso+/:-...\`\`\`\`\`\`...-:/+oosyyyyyyyyyyyys:
+          \`/syyyyyyyssyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyssssssyyyyyyyyyyyyyyyyyyyyy+.
+            .+yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo-
+              -oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo:\`
+                -+syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo-\`
+                  ./syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys+.
+                     -+syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys+:\`
+                       \`-/syyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys+-\`
+                           \`-/oyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys+:.
+                               \`.:/osyyyyyyyyyyyyyyyyyyyyyyyyyyso+:-\`
+                                     \`.-::/++ooosssssooo+//:-.`;
+									 
+	console.log('%c'+img, 'color: #4b4286');
+}());
+
